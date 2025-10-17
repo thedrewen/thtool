eb2f5788a037a4387964fafb44fb573e54c62f40
CL TheDreWen <louis.charneux@gmail.com>
2025-10-17 08:20:15 +0200

    First test

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("./test");
+(0, test_1.test)();
---
+++ dist/test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test = test;
+function test() {
+    console.log('> This is a test ! 💡');
+}
