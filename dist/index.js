3d08c232acfdad16c7d504f366514cd70a96c32c
CL TheDreWen <114508680+thedrewen@users.noreply.github.com>
2025-10-17 09:14:15 +0200

    Delete dist directory

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = void 0;
-var test_1 = require("./test");
-Object.defineProperty(exports, "test", { enumerable: true, get: function () { return test_1.test; } });
--- dist/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = test;
-function test() {
-    console.log('> This is a test ! 💡');
-}
